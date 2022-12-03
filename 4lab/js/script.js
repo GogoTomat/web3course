a95bf21b67ed51979d874f98f2f81f4e9463583a
Mark Komissarov <mkomissarov207@gmail.com>
2022-12-03 10:56:26 +0500

    add 4lab

---
+++ 4lab/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('click', function(event) {
+    if (event.target.dataset.counter !== undefined) { // если есть атрибут...
+        event.target.value++;
+    }
+});
+
+document.addEventListener('click', function(event) {
+    let id = event.target.dataset.toggleId;
+    if (!id) return;
+
+    let elem = document.getElementById(id);
+
+    elem.hidden = !elem.hidden;
+});
